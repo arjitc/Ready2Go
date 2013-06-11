680c7feae7c37021c2b4d83559c7596c247b8f27
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 23:34:40 +0530

    added second warning message

--- update.sh
+++ update.sh
@@ -8,7 +8,11 @@ read -p "press [enter] to continue..."
 cd Ready2Go-sys && rm -rf cgminer
 wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
 chmod +x build-cgminer.sh
+echo ---------------------------------
 echo Update starting
+echo ---------------------------------
+echo Note, this will take some time as it builds CGminer fresh from git!
+echo ---------------------------------
 wait 2
 ./build-cgminer.sh
 echo Update done.
