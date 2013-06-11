70376f1c105d5ac07ee5ff1b962f8b35cdfae169
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 23:33:02 +0530

    build time warning message added

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 echo ---------------------------------
 echo Welcome to the Ready2Go updater
 echo ---------------------------------
+echo Note, this will take some time as it builds CGminer fresh from git!
+echo ---------------------------------
 read -p "press [enter] to continue..."
 cd Ready2Go-sys && rm -rf cgminer
 wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
