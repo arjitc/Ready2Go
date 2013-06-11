bd64b980bf5b8babe9ba2dc932b03afdede4913b
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 23:37:29 +0530

    stats file (basic system stuff for now)

---
+++ stats.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo ---------------------------------
+echo        Ready2Go Stats
+echo ---------------------------------
+echo Uptime :
+echo ---------------------------------
+echo uptime
+echo ---------------------------------
+echo RAM usage : 
+echo ---------------------------------
+echo free -m
+echo ---------------------------------
+echo Disk usage : 
+echo ---------------------------------
+echo df -h
+echo ---------------------------------
+read -p "press [enter] to continue..."
+clear
