e1ad6c7ae5ef0bc2efe6971e1837df388fa1bf54
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 23:32:10 +0530

    Minor UI changes

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo ---------------------------------
 echo Welcome to the Ready2Go updater
+echo ---------------------------------
 read -p "press [enter] to continue..."
 cd Ready2Go-sys && rm -rf cgminer
 wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
