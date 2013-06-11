86c5bb7cd0db2acae553bd976bb0bfcb0ceb3561
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 17:06:52 +0530

    Update build-cgminer.sh
    missed a lot of sudo's

--- build-cgminer.sh
+++ build-cgminer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo Updating system
 sleep 3
-apt-get update 
+sudo apt-get update 
 echo Installing dependencies
 sleep 3
-apt-get install git
-apt-get install libcurl4-openssl-dev
-apt-get install libncurses5-dev
-apt-get install libtool
-apt-get install libusb-1.0
+sudo apt-get install git
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libncurses5-dev
+sudo apt-get install libtool
+sudo apt-get install libusb-1.0
 git clone https://github.com/ckolivas/cgminer.git
 cd cgminer
 ./autogen.sh --enable-bflsc  --enable-bitforce --enable-icarus --enable-modminer --enable-ztex   --enable-avalon
