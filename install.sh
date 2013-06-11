9ba0f3432f0b41a8b8270785bb14d346ede63064
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 15:52:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ alias cgminer="cgminer"
 echo ---------------------------------
 echo Install done !
 echo Two things to remember -for now-
-echo 1. To update your cgminer version type "update" and it'll update
-echo 2. To start mining type "cgminer" and cgminer will launch
+echo 1. To update your cgminer version type   update   and it'll update
+echo 2. To start mining type   cgminer   and cgminer will launch
 echo ---------------------------------
 
 
