c63460ef4a81372ab13187d5de844a3b1d1be054
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 15:22:51 +0530

    Update build-cgminer.sh
    Added libusb-1.0 support

--- build-cgminer.sh
+++ build-cgminer.sh
@@ -6,6 +6,7 @@ apt-get install git
 apt-get install libcurl4-openssl-dev
 apt-get install libncurses5-dev
 apt-get install libtool
+apt-get install libusb-1.0
 git clone https://github.com/ckolivas/cgminer.git
 cd cgminer
 ./autogen.sh --enable-bflsc  --enable-bitforce --enable-icarus --enable-modminer --enable-ztex   --enable-avalon
