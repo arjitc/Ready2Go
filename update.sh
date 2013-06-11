c767043c4ce5a67ba6a564c589de679669fa0ca2
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 15:37:38 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Welcome to the Ready2Go updater
 read -p "press [enter] to continue..."
-mkdir Ready2Go-update && cd Ready2Go-update
+cd Ready2Go-sys
 wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
 chmod +x build-cgminer.sh
 echo Update starting
