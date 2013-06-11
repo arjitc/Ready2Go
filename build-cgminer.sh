2c5688f2a131ae1c7d6b2014d7a6d3d2c48ab16e
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 15:25:17 +0530

    Update build-cgminer.sh
    Make && sudo make install

--- build-cgminer.sh
+++ build-cgminer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo Updating system
+sleep 3
 apt-get update 
 echo Installing dependencies
+sleep 3
 apt-get install git
 apt-get install libcurl4-openssl-dev
 apt-get install libncurses5-dev
@@ -10,3 +12,4 @@ apt-get install libusb-1.0
 git clone https://github.com/ckolivas/cgminer.git
 cd cgminer
 ./autogen.sh --enable-bflsc  --enable-bitforce --enable-icarus --enable-modminer --enable-ztex   --enable-avalon
+make && sudo make install
