c1a2aa18cef2d779415b58ab488620694efd9eb2
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-11 15:01:51 +0530

    Create build-cgminer.sh
    Basic script build, libusb / libudev missing

---
+++ build-cgminer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Updating system
+apt-get update 
+echo Installing dependencies
+apt-get install git
+apt-get install libcurl4-openssl-dev
+apt-get install libncurses5-dev
+apt-get install libtool
+git clone https://github.com/ckolivas/cgminer.git
+cd cgminer
+./autogen.sh --enable-bflsc  --enable-bitforce --enable-icarus --enable-modminer --enable-ztex   --enable-avalon
