566543fcf369c342fb878e70f0d29f6952a40c08
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-01 23:33:50 +0530

    Added --no-check-certificate to wget

--- system-update.sh
+++ system-update.sh
@@ -4,8 +4,10 @@ echo ----------------------
 echo !This will take time!
 echo ----------------------
 read -p "press [enter] to continue..."
-apt-get update && apt-get dist-upgrade
+apt-get update && apt-get -y dist-upgrade
 rm -rf update.sh && rm -rf stats.sh && rm -rf .bashrc
-wget https://raw.github.com/arjitc/Ready2Go/master/update.sh
-wget https://raw.github.com/arjitc/Ready2Go/master/.bashrc
-wget https://raw.github.com/arjitc/Ready2Go/master/stats.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/update.sh --no-check-certificate
+wget https://raw.github.com/arjitc/Ready2Go/master/.bashrc --no-check-certificate
+wget https://raw.github.com/arjitc/Ready2Go/master/stats.sh --no-check-certificate
+clear
+echo System updated!
