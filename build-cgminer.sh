0cbead5607bd2affe5b96b7851995ee1ab6eb73d
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-01 23:31:52 +0530

    Added -y to apt-get

--- build-cgminer.sh
+++ build-cgminer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo Updating system
-sleep 3
+sleep 2
 sudo apt-get update 
 echo Installing dependencies
-sleep 3
-sudo apt-get install git
-sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install libncurses5-dev
-sudo apt-get install libtool
-sudo apt-get install libusb-1.0
-sudo apt-get install dh-autoreconf
+sleep 2
+sudo apt-get -y install git
+sudo apt-get -y install libcurl4-openssl-dev
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install libtool
+sudo apt-get -y install libusb-1.0
+sudo apt-get -y install dh-autoreconf
 git clone https://github.com/ckolivas/cgminer.git
 cd cgminer
 sudo ./autogen.sh --enable-bflsc  --enable-bitforce --enable-icarus --enable-modminer --enable-ztex   --enable-avalon
