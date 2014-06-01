b38415710f271bc7908948e1783de2b1006d2508
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-01 23:36:42 +0530

    Added --no-check-certificate to wget

--- install.sh
+++ install.sh
@@ -7,20 +7,20 @@ echo ---------------------------------
 read -p "press [enter] to continue install or ctrl+C to exit..."
 mkdir Ready2Go-sys && cd Ready2Go-sys
 rm -rf build-cgminer.sh
-wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh --no-check-certificate
 chmod +x build-cgminer.sh
 ./build-cgminer.sh
 echo ---------------------------------
 echo Install [2/4] ...
 echo ---------------------------------
 wait 2
-wget https://raw.github.com/arjitc/Ready2Go/master/update.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/update.sh --no-check-certificate
 chmod +x update.sh
 echo ---------------------------------
 echo Install [3/4] ...
 echo ---------------------------------
 wait 2
-wget https://raw.github.com/arjitc/Ready2Go/master/build-motd.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/build-motd.sh --no-check-certificate
 chmod +x build-motd.sh
 ./build-motd.sh
 echo ---------------------------------
@@ -28,7 +28,7 @@ echo Install [4/4] ...
 echo ---------------------------------
 wait 2
 rm -rf .bashrc
-wget https://raw.github.com/arjitc/Ready2Go/master/.bashrc
+wget https://raw.github.com/arjitc/Ready2Go/master/.bashrc --no-check-certificate
 echo ---------------------------------
 echo Install done !
 echo ---------------------------------
