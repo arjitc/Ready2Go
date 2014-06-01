2e00e2ac1f04307a2b92222455943d98458a3025
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-01 23:35:42 +0530

    Added a few "clear".

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ read -p "press [enter] to continue..."
 cd Ready2Go-sys && rm -rf cgminer
 wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
 chmod +x build-cgminer.sh
+clear
 echo ---------------------------------
 echo Update starting
 echo ---------------------------------
@@ -15,5 +16,6 @@ echo Note, this will take some time as it builds CGminer fresh from git!
 echo ---------------------------------
 wait 2
 ./build-cgminer.sh
+clear
 echo Update done.
 read -p "press [enter] to continue..."
