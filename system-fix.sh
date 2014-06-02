bf013bacbbd6ff931abc8557205e8cab2988a2d9
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-02 17:42:12 +0530

    Added --no-check-certificate to wget

--- system-fix.sh
+++ system-fix.sh
@@ -8,16 +8,16 @@ read -p "press [enter] to continue or ctrl+C to exit..."
 rm -rf Ready2Go-sys
 mkdir Ready2Go-sys && cd Ready2Go-sys
 rm -rf build-cgminer.sh
-wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/build-cgminer.sh --no-check-certificate
 chmod +x build-cgminer.sh
 ./build-cgminer.sh
-wget https://raw.github.com/arjitc/Ready2Go/master/update.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/update.sh --no-check-certificate
 chmod +x update.sh
-wget https://raw.github.com/arjitc/Ready2Go/master/build-motd.sh
+wget https://raw.github.com/arjitc/Ready2Go/master/build-motd.sh --no-check-certificate
 chmod +x build-motd.sh
 ./build-motd.sh
 rm -rf .bashrc
-wget https://raw.github.com/arjitc/Ready2Go/master/.bashrc
+wget https://raw.github.com/arjitc/Ready2Go/master/.bashrc --no-check-certificate
 apt-get update && apt-get dist-upgrade
 echo ---------------------------------
 echo System fix done ! Reboot RPi to make sure changes applied.
